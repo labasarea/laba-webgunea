6da4ef5eb80fb4dc358639abe4df2878bcad72b1
Aitor Urrutia <aitor.urrutia@biko2.com>
2022-01-14 19:42:29 +0100

    feat: hasiera sortzen du

---
+++ back/src/api/hasiera/controllers/hasiera.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  hasiera controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::hasiera.hasiera');
---
+++ back/src/api/hasiera/routes/hasiera.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hasiera router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::hasiera.hasiera');
