ae6e5232156a7d34c458427b8fc3d58fc289de0f
Aitor Urrutia <aitor.urrutia@biko2.com>
2022-01-14 20:18:37 +0100

    feat: gatsby strapirekin konektatzen du

---
+++ front/src/pages/404.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const NotFoundPage: React.VFC = () => {
+  return (
+    <main>
+      
+      <h1 >Ez dugu orria topatu</h1>
+      
+        <br />
+        <Link to="/">Go home</Link>.
+    </main>
+  )
+}
+
+export default NotFoundPage
---
+++ front/src/pages/index.tsx
@@ -0,0 +1,34 @@
+import { graphql, PageProps, useStaticQuery } from "gatsby"
+import React from "react"
+
+interface DataProps {
+  strapiHasiera: {
+    data: {
+      attributes: {
+        deskribapena: string
+        izenburua: string
+      }
+    }
+  }
+  
+}
+
+const IndexPage: React.VFC<PageProps<DataProps>> = () => {
+  const {strapiHasiera} = useStaticQuery<DataProps>(graphql`
+  {
+    strapiHasiera {
+      data {
+        attributes {
+          deskribapena
+          izenburua
+        }
+      }
+    }
+  }
+  `)
+  return (
+    <main><h1>{strapiHasiera.data.attributes.izenburua}</h1><p>{strapiHasiera.data.attributes.deskribapena}</p></main>
+  )
+}
+
+export default IndexPage
