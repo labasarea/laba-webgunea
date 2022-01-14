3d8695864760041dbee1483aa9c43134a1d07528
Aitor Urrutia <aitor.urrutia@biko2.com>
2022-01-14 20:28:45 +0100

    chore: prettier erabiltzen du

--- front/gatsby-config.js
+++ front/gatsby-config.js
@@ -1,39 +1,39 @@
-require("dotenv").config({
-    path: `.env`,
-  })
-  
-  module.exports = {
-    plugins: [
-      "gatsby-plugin-react-helmet",
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          name: `images`,
-          path: `${__dirname}/src/images`,
-        },
+require('dotenv').config({
+  path: `.env`,
+});
+
+module.exports = {
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
-      {
-        resolve: "gatsby-source-strapi",
-        options: {
-          apiURL: process.env.API_URL || "http://localhost:1337/api",
-          singleTypes: ["hasiera"],
-          queryLimit: 1000,
-        },
+    },
+    {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: process.env.API_URL || 'http://localhost:1337/api',
+        singleTypes: ['hasiera'],
+        queryLimit: 1000,
       },
-      "gatsby-transformer-sharp",
-      "gatsby-plugin-sharp",
-      {
-        resolve: `gatsby-plugin-manifest`,
-        options: {
-          name: "gatsby-starter-default",
-          short_name: "starter",
-          start_url: "/",
-          background_color: `#663399`,
-          theme_color: `#663399`,
-          display: `minimal-ui`,
-          icon: `src/images/favicon.png`,
-        },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`,
       },
-      "gatsby-plugin-sitemap"
-    ],
-  }
+    },
+    'gatsby-plugin-sitemap',
+  ],
+};
