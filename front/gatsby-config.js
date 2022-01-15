cf4b202dbf43e10330f0d00c58d3a3ac1cb298df
Aitor Urrutia <aitor.urrutia@biko2.com>
2022-01-15 22:05:17 +0100

    produkziorako konfigurazioa sortzen du

--- front/gatsby-config.js
+++ front/gatsby-config.js
@@ -1,5 +1,5 @@
 require('dotenv').config({
-  path: `.env`,
+  path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
@@ -22,18 +22,18 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`,
-      },
-    },
-    'gatsby-plugin-sitemap',
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: 'gatsby-starter-default',
+    //     short_name: 'starter',
+    //     start_url: '/',
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/favicon.png`,
+    //   },
+    // },
+    // 'gatsby-plugin-sitemap',
   ],
 };
