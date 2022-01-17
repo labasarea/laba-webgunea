7f5fd87c43d9b815ae468186e11ffc32a7a9f23f
Aitor Urrutia <aitor.urrutia@biko2.com>
2022-01-17 14:42:12 +0100

    fix: prop izena zuzentzen du

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -61,13 +61,13 @@ const IndexPage: React.VFC<PageProps> = () => {
           <SareSozialak>
             <SareSoziala>
               <a href="https://twitter.com/labasarea/">
-                <Twitter tabindex="0" role="link" title="Laba Twitterren" />
+                <Twitter tabIndex="0" role="link" title="Laba Twitterren" />
               </a>
             </SareSoziala>
 
             <SareSoziala>
               <a href="https://www.instagram.com/labasarea/">
-                <Instagram tabindex="0" role="link" title="Laba Instagramen" />
+                <Instagram tabIndex="0" role="link" title="Laba Instagramen" />
               </a>
             </SareSoziala>
           </SareSozialak>
