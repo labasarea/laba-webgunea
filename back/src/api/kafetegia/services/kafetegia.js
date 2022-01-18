9aab8c0fa6d9ebb6dc70d2d9e15cbedc1466edc4
Aitor Urrutia <aitor.urrutia@biko2.com>
2022-01-18 18:07:36 +0100

    feat: backean kafetegia sortzen du

---
+++ back/src/api/kafetegia/services/kafetegia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kafetegia service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::kafetegia.kafetegia');
