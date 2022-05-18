0d44126d12376cf21f03dc97ee5ae4248147be37
Aitor Urrutia Aranburu <aitorcurrutia@gmail.com>
2022-05-18 20:40:26 +0200

    refactor: extract helper

--- e2e/cypress/integration/hasiera.spec.ts
+++ e2e/cypress/integration/hasiera.spec.ts
@@ -1,17 +1,17 @@
 describe("Hasiera", () => {
   it("hasiera orria erakusten du", () => {
-    cy.visit("/");
+    visitHasiera();
 
     cy.findByRole("heading", { name: /laba gara/i });
   });
 
   it("Twitter-era bideratzen du", () => {
-    cy.intercept("https://www.instagram.com/labasarea/", {
+    cy.intercept("https://twitter.com/labasarea/", {
       statusCode: 200,
       body: "<h1>Kanpoko web orria</h1>",
     });
 
-    cy.visit("/");
+    visitHasiera();
     cy.findByRole("link", { name: /laba twitterren/i }).click();
 
     cy.url().should("eq", "https://twitter.com/labasarea/");
@@ -23,16 +23,19 @@ describe("Hasiera", () => {
       body: "<h1>Kanpoko web orria</h1>",
     });
 
-    cy.visit("/");
+    visitHasiera();
     cy.findByRole("link", { name: /laba instagramen/i }).click();
 
     cy.url().should("eq", "https://www.instagram.com/labasarea/");
   });
 
   it("kafetegira bideratzen du", () => {
-    cy.visit("/");
+    visitHasiera();
     cy.findByRole("link", { name: /dastatu laba/i }).click();
 
     cy.location("pathname").should("include", "/kafetegia");
   });
 });
+function visitHasiera() {
+  cy.visit("/");
+}
