dece814faba45e3bd64ade34c3935bbe071e0e76
Aitor Urrutia Aranburu <aitorcurrutia@gmail.com>
2023-06-26 19:15:56 +0200

    feat: netlify deployments sartzen du

--- back/config/plugins.js
+++ back/config/plugins.js
@@ -11,4 +11,19 @@ module.exports = {
       },
     },
   },
+  "netlify-deployments": {
+    enabled: true,
+    config: {
+      accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+      sites: [
+        {
+          name: "laba-webgunea",
+          id: "3e9ed70d-2f4b-4152-9aaf-1129570d01b4",
+          buildHook:
+            "https://api.netlify.com/build_hooks/61e338c09f4ef3bf644f86db",
+          branch: "main", // optional
+        },
+      ],
+    },
+  },
 };
