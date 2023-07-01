85961e9a39f2e9489338c75c5ad2677190830f7e
Aitor Urrutia Aranburu <aitorcurrutia@gmail.com>
2023-07-01 10:28:59 +0200

    feat: Strapi bertsioa igotzen du eta sf-ekitaldia sortzen du

---
+++ back/src/api/sf-ekitaldia/routes/sf-ekitaldia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sf-ekitaldia router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::sf-ekitaldia.sf-ekitaldia');
---
+++ back/src/api/sf-ekitaldia/services/sf-ekitaldia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sf-ekitaldia service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::sf-ekitaldia.sf-ekitaldia');
