cc0f487f48a57d909b31dd41c88a7eb4f22207ba
Aitor Urrutia Aranburu <aitorcurrutia@gmail.com>
2023-12-24 13:27:19 +0100

    refactor: kontaktu datuak batzen ditu

--- front/src/components/Gainburua/KontaktuDatuak.tsx
+++ front/src/components/Gainburua/KontaktuDatuak.tsx
@@ -2,19 +2,39 @@ import React from 'react';
 
 import InstagramLogo from '../../assets/instagram.svg';
 import TwitterLogo from '../../assets/twitter.svg';
+import { classNames } from '../../utilities/classnames';
 import * as styles from './KontaktuDatuak.module.scss';
 
-/**
- * ERREPIKATUA
- * KontaktuDatuak konponentea MugikorNabigazioan errepikatzen da, honek
- * logoaren fill-erako kokapenaren araberako CSS aldagaia erabiltzen duelako
- * TODO: Beste estrategia bat bilatu, abstrakzio bakarra izateko
- */
+interface Props {
+  kolorea?: KontaktuaColor;
+}
 
-export const KontaktuDatuak: React.FC = () => {
+type KontaktuaColor = 'theme' | 'zuria';
+type ColorToStyleDictionary = {
+  [color in KontaktuaColor]: string;
+};
+
+const colorToLogoStyle: ColorToStyleDictionary = {
+  theme: styles.themeLogo,
+  zuria: styles.zuriLogo,
+};
+
+const colorToBorderStyle: ColorToStyleDictionary = {
+  theme: styles.themeBorder,
+  zuria: styles.zuriBorder,
+};
+
+const colorToLinkStyle: ColorToStyleDictionary = {
+  theme: styles.themeLink,
+  zuria: styles.zuriLink,
+};
+
+export const KontaktuDatuak: React.FC<Props> = ({ kolorea = 'theme' }) => {
   return (
     <div className={styles.wrapper}>
-      <address className={styles.helbidea}>
+      <address
+        className={classNames(styles.helbidea, colorToBorderStyle[kolorea])}
+      >
         <p>Gazteluko plaza, 2</p>
         <p>Iruñea</p>
       </address>
@@ -22,13 +42,16 @@ export const KontaktuDatuak: React.FC = () => {
       <ul className={styles.sareSozialak}>
         <li className={styles.sareSoziala}>
           <a
-            className={styles.gainburuLink}
+            className={colorToLinkStyle[kolorea]}
             tabIndex={0}
             aria-label="Laba Twitterren"
             href="https://twitter.com/labasarea/"
           >
             <TwitterLogo
-              className={styles.sareSozialLogo}
+              className={classNames(
+                styles.sareSozialLogo,
+                colorToLogoStyle[kolorea],
+              )}
               title="Laba Twitterren"
             />
           </a>
@@ -36,13 +59,16 @@ export const KontaktuDatuak: React.FC = () => {
 
         <li className={styles.sareSoziala}>
           <a
-            className={styles.gainburuLink}
+            className={colorToLinkStyle[kolorea]}
             tabIndex={0}
             aria-label="Laba Instagramen"
             href="https://www.instagram.com/labasarea/"
           >
             <InstagramLogo
-              className={styles.sareSozialLogo}
+              className={classNames(
+                styles.sareSozialLogo,
+                colorToLogoStyle[kolorea],
+              )}
               title="Laba Instagramen"
             />
           </a>
--- front/src/components/Gainburua/MugikorNabigazioa.tsx
+++ front/src/components/Gainburua/MugikorNabigazioa.tsx
@@ -1,15 +1,10 @@
 import React, { useState } from 'react';
 
 import { Link } from 'gatsby';
-import { rem } from 'polished';
-import styled from 'styled-components';
 
-import InstagramLogo from '../../assets/instagram.svg';
-import TwitterLogo from '../../assets/twitter.svg';
-import { font, size } from '../../ui/theme';
-import { colors } from '../../ui/theme/colors';
 import HamburgerIcon from './assets/hamburguer.svg';
 import Itxi from './assets/itxi.svg';
+import { KontaktuDatuak } from './KontaktuDatuak';
 import * as styles from './MugikorNabigazioa.module.scss';
 
 export const MugikorNabigazioa: React.FC = () => {
@@ -35,7 +30,7 @@ export const MugikorNabigazioa: React.FC = () => {
           </nav>
 
           <div className={styles.kontaktuaWrapper}>
-            <KontaktuDatuak />
+            <KontaktuDatuak kolorea="zuria" />
           </div>
         </div>
       )}
@@ -51,84 +46,3 @@ export const MugikorNabigazioa: React.FC = () => {
     </>
   );
 };
-
-/**
- * ERREPIKATUA
- * KontaktuDatuak konponentea errepikatzen da, besteak logoaren fill-erako kokapenaren
- * araberako CSS aldagaia erabiltzen duelako eta hemen testu eta irudi guztiak zuriak izan
- * behar direlako
- * TODO: Beste estrategia bat bilatu, abstrakzio bakarra izateko
- */
-const KontaktuDatuak: React.FC = () => {
-  return (
-    <Kontaktua>
-      <Helbidea>
-        <p>Gazteluko plaza, 2</p>
-        <p>Iruñea</p>
-      </Helbidea>
-
-      <SareSozialak>
-        <SareSoziala>
-          <a
-            tabIndex={0}
-            aria-label="Laba Twitterren"
-            href="https://twitter.com/labasarea/"
-          >
-            <Twitter title="Laba Twitterren" />
-          </a>
-        </SareSoziala>
-
-        <SareSoziala>
-          <a
-            tabIndex={0}
-            aria-label="Laba Instagramen"
-            href="https://www.instagram.com/labasarea/"
-          >
-            <Instagram role="link" title="Laba Instagramen" />
-          </a>
-        </SareSoziala>
-      </SareSozialak>
-    </Kontaktua>
-  );
-};
-
-export const Kontaktua = styled.div`
-  display: flex;
-`;
-
-export const SareSoziala = styled.li`
-  width: ${rem(size.base)};
-  height: ${rem(size.base)};
-  :not(:last-child) {
-    margin-right: ${rem(size.small)};
-  }
-`;
-
-export const Instagram = styled(InstagramLogo)`
-  width: ${rem(size.base)};
-  height: ${rem(size.base)};
-  path {
-    fill: ${colors.zuria};
-  }
-`;
-
-export const Twitter = styled(TwitterLogo)`
-  width: ${rem(size.base)};
-  height: ${rem(size.base)};
-  path {
-    fill: ${colors.zuria};
-  }
-`;
-
-export const SareSozialak = styled.ul`
-  display: flex;
-  align-items: center;
-  list-style-type: none;
-`;
-
-export const Helbidea = styled.address`
-  border-right: 3px solid ${colors.zuria};
-  padding-right: ${rem(size.small)};
-  margin-right: ${rem(size.small)};
-  ${font.tiny()};
-`;
