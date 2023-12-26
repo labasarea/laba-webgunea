687ddafa7c88da3bb0f781b300af86adb16e0dfb
Aitor Urrutia Aranburu <aitorcurrutia@gmail.com>
2023-12-26 11:20:41 +0100

    feat: Orrialde basikoa CSS modulesera pasatzen du

--- front/src/views/RegularPage/RegularPage.tsx
+++ front/src/views/RegularPage/RegularPage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
-import { rem } from 'polished';
 import remarkGfm from 'remark-gfm';
-import { styled } from 'styled-components';
 
 import { Container } from '../../components/Container';
 import { Gainburua } from '../../components/Gainburua';
 import { Oina } from '../../components/Oina';
-import { colors, font, fontWeight, size } from '../../ui/theme';
+import * as styles from './RegularPage.module.scss';
 import { RegularPageContent } from './RegularPageContent';
 
 interface Props {
@@ -23,76 +21,14 @@ export const RegularPage: React.FC<Props> = ({ content }) => {
       <Gainburua />
 
       <Container>
-        <Atala>
-          <Edukia>{edukia}</Edukia>
-        </Atala>
+        <section className={styles.atala}>
+          <ReactMarkdown remarkPlugins={[remarkGfm]} className={styles.edukia}>
+            {edukia}
+          </ReactMarkdown>
+        </section>
       </Container>
 
       <Oina />
     </>
   );
 };
-
-const Atala = styled.section`
-  padding-top: ${rem(size.base)};
-`;
-
-const Edukia = styled(ReactMarkdown).attrs({ remarkPlugins: [remarkGfm] })`
-  font-weight: ${fontWeight.regular};
-  ${font.base()};
-
-  p:not(-last-child) {
-    margin-bottom: ${rem(size.base)};
-  }
-
-  h1 {
-    ${font.gargantuan()};
-    font-weight: ${fontWeight.bold};
-    margin-bottom: ${rem(size.base)};
-  }
-
-  h2 {
-    ${font.huge()};
-    font-weight: ${fontWeight.bold};
-    margin-bottom: ${rem(size.base)};
-  }
-
-  h3 {
-    ${font.large()};
-    font-weight: ${fontWeight.bold};
-    margin-bottom: ${rem(size.base)};
-  }
-
-  h4 {
-    font-weight: ${fontWeight.bold};
-    margin-bottom: ${rem(size.mini)};
-  }
-
-  ul {
-    margin-bottom: ${rem(size.base)};
-  }
-
-  li {
-    list-style: disc inside;
-    margin-bottom: ${rem(size.small)};
-  }
-
-  table {
-    margin-bottom: ${rem(size.base)};
-  }
-
-  th {
-    padding: ${size.small}px;
-  }
-
-  td {
-    padding: ${size.small}px;
-    border: 1px solid ${colors.beltza};
-  }
-
-  hr {
-    border: 2px solid ${colors.beltza};
-    margin-bottom: ${rem(size.base)};
-    margin-top: ${rem(size.base)};
-  }
-`;
