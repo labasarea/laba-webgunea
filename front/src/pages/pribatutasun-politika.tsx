c1b2a73492c9e0b0386b3547ff9b211d1604bb4f
Aitor Urrutia Aranburu <aitorcurrutia@gmail.com>
2023-12-26 10:12:56 +0100

    refactor: use comonent

--- front/src/pages/pribatutasun-politika.tsx
+++ front/src/pages/pribatutasun-politika.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { graphql, PageProps, useStaticQuery } from 'gatsby';
 
 import { SEO } from '../components/SEO';
-import { GlobalStyles } from '../ui/GlobalStyles';
 import { RegularPage } from '../views/RegularPage/RegularPage';
 import { RegularPageContent } from '../views/RegularPage/RegularPageContent';
 
@@ -24,8 +23,6 @@ const PribatutasunPolitika: React.FC<PageProps> = ({ location }) => {
   return (
     <>
       <SEO title={strapiPribatutasunPolitika.izenburua} location={location} />
-
-      <GlobalStyles />
 
       <RegularPage content={strapiPribatutasunPolitika} />
     </>
