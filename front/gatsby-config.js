a825085856f39d39c092385c45c29b0a3534ca7c
Aitor Urrutia Aranburu <aitorcurrutia@gmail.com>
2023-12-27 09:03:04 +0100

    fix: build-a konpontzen du

--- front/gatsby-config.js
+++ front/gatsby-config.js
@@ -7,13 +7,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
       resolve: 'gatsby-source-strapi-graphql',
       options: {
         apiURL: process.env.API_URL,
