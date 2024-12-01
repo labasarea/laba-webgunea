166fb859036ad311d1e49430d7222999071d5cb2
Aitor Urrutia Aranburu <aitorcurrutia@gmail.com>
2024-12-01 12:57:13 +0100

    chore: netlify deployments kentzen du

--- back/config/plugins.js
+++ back/config/plugins.js
@@ -25,19 +25,19 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  "netlify-deployments": {
-    enabled: true,
-    config: {
-      accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
-      sites: [
-        {
-          name: "laba-webgunea",
-          id: "3e9ed70d-2f4b-4152-9aaf-1129570d01b4",
-          buildHook:
-            "https://api.netlify.com/build_hooks/61e338c09f4ef3bf644f86db",
-          branch: "main", // optional
-        },
-      ],
-    },
-  },
+  // "netlify-deployments": {
+  //   enabled: true,
+  //   config: {
+  //     accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+  //     sites: [
+  //       {
+  //         name: "laba-webgunea",
+  //         id: "3e9ed70d-2f4b-4152-9aaf-1129570d01b4",
+  //         buildHook:
+  //           "https://api.netlify.com/build_hooks/61e338c09f4ef3bf644f86db",
+  //         branch: "main", // optional
+  //       },
+  //     ],
+  //   },
+  // },
 });
