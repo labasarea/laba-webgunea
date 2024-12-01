393d5aac669e5db47010fb88127a447e9257a4ed
Aitor Urrutia Aranburu <aitorcurrutia@gmail.com>
2024-12-01 12:07:05 +0100

    chore: konfigurazioa aldatzen du

--- back/config/env/production/server.js
+++ back/config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: env("PUBLIC_URL", "https://fast-springs-77208.herokuapp.com"),
+  url: env("PUBLIC_URL", "https://vervet-big-dassie.ngrok-free.app"),
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
 });
