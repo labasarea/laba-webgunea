9e9dfc134a102a183857db7fa04430c2fa7b24ed
Aitor C. Urrutia Aranburu <aitorcurrutia@gmail.com>
2025-08-27 18:08:52 +0200

    feat: sitemap-a sortzen du

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -4,10 +4,13 @@ import node from "@astrojs/node";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "http://localhost:3000",
   output: "static",
 
   trailingSlash: "never",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
